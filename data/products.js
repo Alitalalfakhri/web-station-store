7644820db681b82d60927330e1a6d619cabaf446
alitalalfakhri <ali.talalfakhri0009@gmail.com>
2024-07-26 16:34:31 +0300

    one hour of nothing

--- data/products.js
+++ data/products.js
@@ -1,4 +1,5 @@
-import { formatCurrency } from "../scripts/utils/money.js";
+import {formatCurrency} from '../scripts/utils/money.js';
+
 export function getProduct(productId) {
   let matchingProduct;
 
@@ -10,26 +11,53 @@ export function getProduct(productId) {
 
   return matchingProduct;
 }
-class Products {
+
+class Product {
   id;
   image;
   name;
   rating;
   priceCents;
-  constructor(productsDetails) {
-    this.id = productsDetails.id;
-    this.image = productsDetails.image;
-    this.name = productsDetails.name;
-    this.rating = productsDetails.rating;
-    this.priceCents = productsDetails.priceCents;
+
+  constructor(productDetails) {
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
   }
-   getStarsUrl() {
+
+  getStarsUrl() {
     return `images/ratings/rating-${this.rating.stars * 10}.png`;
   }
-   getPrice(){
-    return ` $${formatCurrency(this.priceCents)}`
-   }
+
+  getPrice() {
+    return `$${formatCurrency(this.priceCents)}`;
+  }
+
+  extraInfoHTML() {
+    return '';
+  }
 }
+
+class Clothing extends Product {
+  sizeChartLink;
+
+  constructor(productDetails) {
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML() {
+    // super.extraInfoHTML();
+    return `
+      <a href="${this.sizeChartLink}" target="_blank">
+        Size chart
+      </a>
+    `;
+  }
+}
+
 
 export const products = [
   {
@@ -690,6 +718,9 @@ export const products = [
       "mens"
     ]
   }
-].map((productsDetails) => {
-  return  new Products(productsDetails);
-})
+].map((productDetails) => {
+  if (productDetails.type === 'clothing') {
+    return new Clothing(productDetails);
+  }
+  return new Product(productDetails);
+});
