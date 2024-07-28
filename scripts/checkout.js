f005d12e7ec67cfc2e97c8b9725989e23f279ab7
alitalalfakhri <ali.talalfakhri0009@gmail.com>
2024-07-28 19:38:28 +0300

    practice on back-end

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,5 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymetSummary.js';
-
+// import '../data/backend-practice.js'
 renderOrderSummary();
 renderPaymentSummary();
